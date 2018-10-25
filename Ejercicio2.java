5b100283d5925797f683a128e4fb5ed06903dd53
RacsoRF <oscar_adrian9@hotmail.com>
2018-10-25 17:53:49 -0600

    first commit

---
+++ Ejercicio2.java
@@ -0,0 +1,19 @@
+
+public class Ejercicio2{
+
+	int num2;
+		public static void main (String args []){
+			for (int i = 1; i <=100; i++){
+
+
+			if (i % 2 ==0){	
+				System.out.println(i);
+
+}
+			else if (i%3 == 0){
+				System.out.println(i);
+				}
+			}		
+		}
+	}
+
